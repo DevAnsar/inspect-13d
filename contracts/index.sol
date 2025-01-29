85c5b4de637b2feb90e42787cc999df2f20f2e6e
DevAnsar <ansarmirzayi@gmail.com>
2025-01-29 14:08:32 +0000

    update contarct

--- contracts/index.sol
+++ contracts/index.sol
@@ -1,1 +1,10 @@
-//main contract code
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DjunoxToken is ERC20 {
+    constructor() ERC20("Djunox", "DJX") {
+        _mint(msg.sender, 100000 * 10 ** decimals());
+    }
+}
